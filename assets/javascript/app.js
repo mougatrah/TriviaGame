f061bbb2b3563a40bd39a30f4e126d4b13ce97f3
workstation1@DOMAIN.LOCAL <workstation1@DOMAIN.LOCAL>
2018-10-18 17:00:20 -0700

    did alot, still buggy

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,7 @@
 var game = {
     currentQ: 0,
+    answersCorrect: 0,
+    answersWrong: 0,
     counting: false,
     questions: [
         {
@@ -16,47 +18,61 @@ var game = {
 
     ],
 
-    setCountdown(){
-
-        
-        console.log("seting up countdown");
-        var time = 10;
-        var id = setInterval(count, 1000);
-        
-        if(game.counting){
-            cancel();
-            game.setCountdown();
-        }
-
-        function count(){
-          
-            if(time == 0){
-                console.log("done");
-                clearInterval(id);
-                game.counting = false;
-                game.timeOut();
-            }else{
-                game.counting = true;
-                console.log('counting');
-                time--;
-                $("#time").text(time);
+    answerChosen(elem) {
+        if (game.questions[game.currentQ - 1]) {
+            if (elem.text() == game.questions[game.currentQ - 1].c) {
+                game.answersCorrect++;
+            } else {
+                game.answersWrong++;
             }
-        };
+            $("#splashImg").src;
+            $("#splashMessage").text("You chose " + elem.text());
 
-        function cancel(){
 
-            console.log("CANCELLING");
-            clearInterval(id);
-            game.counting = false;
+            $("#answer").text("Answer: " + game.questions[game.currentQ - 1].c);
         }
+        $("#splash").show();
+        $("#main").hide();
+        var id = setTimeout(game.changeQ, 5000);
+    },
+
+    endGame() {
+        console.log("ending game");
+        $("#splashMessage").text("GAME OVER. Play again?");
+        $("#answer").text("Yes").on("click", game.setup);
+        $("#main").hide();
+        $("#splash").show();
+    },
+
+
+    timeOut() {
+
+        $("#splashImg").src;
+        $("#splashMessage").text("You ran out of time.");
+        if (game.questions[game.currentQ - 1]) {
+            $("#answer").text("Answer: " + game.questions[game.currentQ - 1].c);
+        }
+
+        $("#splash").show();
+        $("#main").hide();
+
+        setTimeout(game.changeQ, 5000);
+
 
     },
 
-    timeOut(){
-        
-        var id = setTimeout(game.changeQ, 5000);
+    setup() {
+        game.currentQ = 0;
+        game.answersCorrect = 0;
+        game.answersWrong = 0;
+        game.counting = false;
 
-        
+        $("#main").hide();
+        $("#splash").show();
+        $("#splashImg").src;
+        $("#splashMessage").text("Would you like to play a game?");
+        $("#answer").addClass("start");
+
     },
 
     setQ(qObject) {
@@ -67,26 +83,68 @@ var game = {
             var a = qObject.a;
 
             $("#question").text(q);
-
             for (let i in a) {
                 $("#" + i.toString()).text(a[i]);
+                console.log($("#" + i.toString()));
+
             }
         }
+
     },
 
     changeQ() {
-        game.setQ(game.questions[game.currentQ]);
+        if (game.currentQ > game.questions.length) {
+
+            game.endGame();
+        }
+        $("#splash").hide();
+        $("#main").show();
+        var id = setTimeout(game.timeOut, 10 * 1000);
+        $(".choice").on("click", function () {
+            console.log($(this) + " Clearing " + id);
+            clearTimeout(id);
+            game.answerChosen($(this));
+        });
         console.log("changing question");
-        game.setCountdown();
+        game.setQ(game.questions[game.currentQ]);
+
         game.currentQ++;
         console.log(game.currentQ > game.questions.length);
-        if (game.currentQ > game.questions.length) {
-            console.log("HGEYE");
-            game.setQ({ q: "BLAH", a: ["bleep", "blue", "l"] })
-        }
+
 
     },
 
 }
 
-game.changeQ();
+$(document).ready(
+    function () {
+
+        game.setup();
+       
+    }
+);
+
+$(document).on("click", ".btn", function (e) {
+    e.preventDefault();
+    if($(this).hasClass("setup")){
+        game.setup();
+    }
+    if ($(this).hasClass("start")) {
+        console.log("start clicked");
+        $(this).removeClass("start");
+        game.changeQ();
+    } else if ($(this).hasClass('choice')) {
+
+    }
+
+
+    if ($(this).hasClass("btn-primary")) {
+
+    } else if ($(this).hasClass("btn-secondary")) {
+        $(this).addClass("btn-primary");
+        $(this).removeClass("btn-secondary");
+    } else if ($(this).hasClass("btn-info")) {
+
+        $(this).removeClass("btn-info");
+    }
+});
